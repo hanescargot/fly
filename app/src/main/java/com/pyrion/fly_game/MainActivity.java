3389f573e9710c77717fc309b1f1fccba16539e7
pyrio <hanescargot@gamil.com>
2021-07-13 01:49:47 +0900

    2021.07.13.pyrion

--- app/src/main/java/com/pyrion/fly_game/MainActivity.java
+++ app/src/main/java/com/pyrion/fly_game/MainActivity.java
@@ -2,36 +2,79 @@ package com.pyrion.fly_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Random random = new Random();
 
-    int targetNum;
-    ImageView targetFlyID = findViewById(R.id.targetFly);
+    int targetFlyNum;
+    ImageView targetFlyIV;
+    ImageView censoredIV;
 
-    int[] flysNumber = new int[6];
-    ImageView[] flysID = new ImageView[6];
+    int[] flysNumber = new int[9];
+    ImageView[] flysIV = new ImageView[9];
+
+    ArrayList<String> targetFlyTagLIst = new ArrayList<>();
+    HashMap<String, Integer> targetFlyTagHashMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Fly이미지 자리 아이디 연결 And 랜덤 플라이 생성
-        targetNum = random.nextInt(6);
-        targetFlyID.setImageResource(R.drawable.fly1 + targetNum);
-        for(int count = 0; count< flysID.length; count++ ) {
-            flysID[count] = findViewById(R.id.fly1 + count);
 
-            flysNumber[count] = random.nextInt(6); //0~5
-            flysID[count].setImageResource(R.drawable.fly1 + flysNumber[count]);
+        targetFlyIV = findViewById(R.id.targetFly);
+        censoredIV = findViewById(R.id.censored);
+
+        for(int count = 0; count< flysIV.length; count++ ) {
+            flysIV[count] = findViewById(R.id.fly1 + count);
+            flysNumber[count] = random.nextInt(6);
+            flysIV[count].setImageResource(R.drawable.fly1 + flysNumber[count]);
+
+            String key = flysIV[count].getTag().toString();
+            targetFlyTagHashMap.put(key, flysNumber[count]);
+
+            flysIV[count].setOnClickListener(flyButtonListener);
         }
+        setTarget();
+
 
     }
+
+    View.OnClickListener flyButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String tag = v.getTag().toString();
+            if(targetFlyTagHashMap.get(tag)==targetFlyNum){
+                v.setVisibility(View.INVISIBLE);
+                targetFlyTagHashMap.remove(tag);
+            }
+            if( !targetFlyTagHashMap.containsValue(targetFlyNum) ){
+
+                try {
+                    censoredIV.setVisibility(View.VISIBLE);
+                    Thread.sleep(1000);//////////////문제:censored가 보여지지않고 멈춤
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                censoredIV.setVisibility(View.INVISIBLE);
+                setTarget();
+            }
+
+        }
+    };
+
+    void setTarget() {
+        targetFlyNum = targetFlyTagHashMap.values().iterator().next();
+        targetFlyIV.setImageResource(R.drawable.fly1 + targetFlyNum);
+    }
+
+
 }
