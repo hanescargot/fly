5d6494c71ac1896249b06e41580e4d3a15e6d165
hanescargot <hanescargot@gmail.com>
2021-07-13 13:00:37 +0900

    mrhi

--- app/src/main/java/com/pyrion/fly_game/MainActivity.java
+++ app/src/main/java/com/pyrion/fly_game/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -15,13 +14,11 @@ public class MainActivity extends AppCompatActivity {
     Random random = new Random();
 
     int targetFlyNum;
-    ImageView targetFlyIV;
-    ImageView censoredIV;
+    ImageView targetFlyIV, censoredIV;
 
     int[] flysNumber = new int[9];
     ImageView[] flysIV = new ImageView[9];
 
-    ArrayList<String> targetFlyTagLIst = new ArrayList<>();
     HashMap<String, Integer> targetFlyTagHashMap = new HashMap<>();
 
     @Override
@@ -29,10 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        targetFlyIV = findViewById(R.id.targetFly);
+        targetFlyIV = findViewById(R.id.target_fly);
         censoredIV = findViewById(R.id.censored);
-
+        //맵 생성
         for(int count = 0; count< flysIV.length; count++ ) {
             flysIV[count] = findViewById(R.id.fly1 + count);
             flysNumber[count] = random.nextInt(6);
@@ -41,37 +37,37 @@ public class MainActivity extends AppCompatActivity {
             String key = flysIV[count].getTag().toString();
             targetFlyTagHashMap.put(key, flysNumber[count]);
 
-            flysIV[count].setOnClickListener(flyButtonListener);
         }
         setTarget();
 
-
     }
 
-    View.OnClickListener flyButtonListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+    //onClick 속성이 부여된 뷰를 클릭하면 자동으로 실행되는 콜백메소드
+    public void flyButtonListener(View v){
             String tag = v.getTag().toString();
             if(targetFlyTagHashMap.get(tag)==targetFlyNum){
                 v.setVisibility(View.INVISIBLE);
                 targetFlyTagHashMap.remove(tag);
-            }
-            if( !targetFlyTagHashMap.containsValue(targetFlyNum) ){
+            }//TODO 타겟이 아닌 image 클릭 했을 때의 Game over
+            if( !targetFlyTagHashMap.containsValue(targetFlyNum) ) {
 
                 try {
                     censoredIV.setVisibility(View.VISIBLE);
-                    Thread.sleep(1000);//////////////문제:censored가 보여지지않고 멈춤
+                    Thread.sleep(1000);//////////////TODO 문제:censored가 보여지지않고 멈춤
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 censoredIV.setVisibility(View.INVISIBLE);
-                setTarget();
+                if (targetFlyTagHashMap.size()==0){
+                    censoredIV.setVisibility(View.VISIBLE);
+                    //TODO 게임 finish : 보상 Image
+                }else{
+                    setTarget();
+                }
             }
+    }
 
-        }
-    };
-
-    void setTarget() {
+    public void setTarget() {
         targetFlyNum = targetFlyTagHashMap.values().iterator().next();
         targetFlyIV.setImageResource(R.drawable.fly1 + targetFlyNum);
     }
